e1b901be41d8cecddad57a64decfebfdbf15dd4a
Tervasroso <tuokivilahti@gmail.com>
2025-01-02 10:49:24 +0200

    installed mysqldb and created MySQL instance

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from flask import Flask
+from flask_mysqldb import MySQL
 
 # create Flask application
 app = Flask(__name__)
 
-#create secret key for flask & session
+# create secret key for flask & session
 app.secret_key = 'philobeddoe'
 
-#configure MYSQL server:
+# configure MySQL server:
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'user'
 app.config['MYSQL_PASSWORD'] = '323768CR10'
 app.config['MYSQL_DB'] = 'diary'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+# create MySQL instance
+mysql = MySQL(app)
