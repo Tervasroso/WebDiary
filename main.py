206b3ece859eb38eff365a395bf31d2116223e79
Tervasroso <tuokivilahti@gmail.com>
2025-01-02 10:44:19 +0200

    added secret key to main.py and configured MYSQL server

--- main.py
+++ main.py
@@ -2,3 +2,13 @@ from flask import Flask
 
 # create Flask application
 app = Flask(__name__)
+
+#create secret key for flask & session
+app.secret_key = 'philobeddoe'
+
+#configure MYSQL server:
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'user'
+app.config['MYSQL_PASSWORD'] = '323768CR10'
+app.config['MYSQL_DB'] = 'diary'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
