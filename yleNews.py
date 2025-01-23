099dba157c31065c4ea894bf6b19fe1ef75fd628
Tervasroso <tuokivilahti@gmail.com>
2025-01-23 10:59:02 +0200

    changed the features parameter on beaautifulsoup

--- yleNews.py
+++ yleNews.py
@@ -3,24 +3,22 @@ import requests
 
 def getNews():
 
-    url = 'https://feeds.yle.fi/uutiset/v1/majorHeadlines/YLE_UUTISET.rss'
+    url = 'https://feeds.yle.fi/uutiset/v1/recent.rss?publisherIds=YLE_UUTISET'
 
     headlines = requests.get(url)
 
-    soup = BeautifulSoup(headlines.text, 'html')
+    soup = BeautifulSoup(headlines.content, features='html.parser')
 
-    item = soup.find_all('item')
+    items = soup.find_all('item')
 
     newsArticles = []
 
-    for i in item:
+    for i in items:
 
         title = i.find('title').text
         description = i.find('description').text
         link = i.find('link').text
-
         article = {'title': title, 'description': description, 'link': link}
-
         newsArticles.append(article)
 
     return newsArticles[0:5]
